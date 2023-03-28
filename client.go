1a3c013ff5bdc143c9591402968b84e44536f0ee
kehwa weng <kehwaweng@ipevodeMacBook-Pro.local>
2023-03-28 17:29:01 +0800

    [server] 修正時間沒顯示, overtime notification

--- client.go
+++ client.go
@@ -1,48 +1,46 @@
 package main
 
-
 import (
-    "fmt"
-    "net"
-    "strings"
+	"fmt"
+	"net"
+	"strings"
 )
 
 func main() {
 
-    conn, err := net.Dial("tcp","localhost:8080")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer conn.Close()
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
 
-    fmt.Println("connect to ", conn.RemoteAddr())
+	fmt.Println("connect to ", conn.RemoteAddr())
 
+	/// listen to server message
+	go func() {
+		buf := make([]byte, 5*1024)
+		for {
+			n, err := conn.Read(buf)
+			if err != nil {
+				return
+			}
+			msg := string(buf[:n])
+			fmt.Print(msg)
+		}
+	}()
 
-    /// listen to server message
-    go func() {
-        buf:= make([]byte, 5 * 1024)
-        for {
-            n, err := conn.Read(buf)
-            if err != nil {
-                return
-            }
-            msg := string(buf[:n])
-            fmt.Print(msg)
-        }
-    }()
-
-    /// sent msg to server
-    for {
-        var msg string
-        _,_ = fmt.Scanf("%s", &msg)
-        msg = strings.Trim(msg, "\r\n")
-        if "exit" == msg {
-            fmt.Printf("close connect")
-            conn.Close()
-            return
-        }
-        _, _ = conn.Write([]byte(msg))
-    }
+	/// sent msg to server
+	for {
+		var msg string
+		_, _ = fmt.Scanf("%s", &msg)
+		msg = strings.Trim(msg, "\r\n")
+		if "exit" == msg {
+			fmt.Printf("close connect")
+			conn.Close()
+			return
+		}
+		_, _ = conn.Write([]byte(msg))
+	}
 
 }
--- server.go
+++ server.go
@@ -1,197 +1,204 @@
 package main
 
-
 import (
-    "fmt"
-    "net"
-    "strings"
-    "time"
+	"fmt"
+	"net"
+	"strings"
+	"time"
 )
+
 func main() {
-    listener, err:= net.Listen("tcp", "localhost:8080")
-    if err != nil { 
-        fmt.Println(err)
-        return
-    }
-    
-    connMap := make(map[net.Conn]string)
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Println(err)
-            continue    
-        }
-        connMap[conn] = conn.RemoteAddr().String()
-        notifyAllNewUserLogin(connMap[conn], connMap)
-        go HandleConn(conn, connMap)
-    }
-}   
+	connMap := make(map[net.Conn]string)
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		connMap[conn] = conn.RemoteAddr().String()
+		notifyAllNewUserLogin(connMap[conn], connMap)
+		go HandleConn(conn, connMap)
+	}
+}
 
-/// handle every connect
+// / handle every connect
 func HandleConn(conn net.Conn, connMap map[net.Conn]string) {
 
-    buf := make([]byte, 1024)
-    /// close conn
-    defer handlerConnClose(conn, connMap, 1)
+	buf := make([]byte, 1024)
+	/// close conn
+	defer handlerConnClose(conn, connMap, 1)
 
-    /// create a thread to monitor overtime
-    keepAlive := make(chan bool)
-    go func(conn net.Conn) {
-        for {
-            select {
-            case <- keepAlive:
-            case <- time.After(1 * time.Minute):
-                /// this connect overtime
-                handlerConnClose(conn, connMap, 2)
-                return
-            }
-        }
-    }(conn)
+	/// create a thread to monitor overtime
+	keepAlive := make(chan bool)
+	go func(conn net.Conn) {
+		for {
+			select {
+			case <-keepAlive:
+			case <-time.After(1 * time.Minute):
+				/// this connect overtime
+				handlerConnClose(conn, connMap, 2)
+				return
+			}
+		}
+	}(conn)
 
-    /// print helper list
-    help(conn)
-    for {
-        n, err:= conn.Read(buf)
-        if err!= nil {
-            return
-        }
-        keepAlive <- true
-        msg := strings.Trim(string(buf[:n]), "\r\n")
-        handleMsg(conn,connMap, msg)
-    }
+	/// print helper list
+	help(conn)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		keepAlive <- true
+		msg := strings.Trim(string(buf[:n]), "\r\n")
+		handleMsg(conn, connMap, msg)
+	}
 }
 
-/// handle close connect
+// / handle close connect
 func handlerConnClose(conn net.Conn, connMap map[net.Conn]string, t int) {
-    /// t = 1: initiative close connect
-    /// t = 2: overtime close connect
-    userExit(conn, connMap, t)
-    _ = conn.Close()
+	/// t = 1: initiative close connect
+	/// t = 2: overtime close connect
+	if t == 2 {
+		fmt.Printf("%s overtime ", connMap[conn])
+		msg := "[" + time.Now().Format("2006-01-02 15:04:05") + "] - " + connMap[conn] + ": " + "overtime logout \n"
+		_, err := conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("failed to send msg to  %s", connMap[conn])
+
+		}
+	}
+	userExit(conn, connMap, t)
+	_ = conn.Close()
 }
 
-/// notify new user login
+// / notify new user login
 func notifyAllNewUserLogin(name string, connMap map[net.Conn]string) {
-    for curConn := range connMap {
-        msg:= "[" +time.Now().Format("00:00:00") + "] - " + name + " login \n"
-        _, err := curConn.Write([]byte(msg))
-        if err != nil {
-            fmt.Printf("failed to remind %s new user loging", connMap[curConn])
-            continue
-        }
-    }
+	for curConn := range connMap {
+		msg := "[" + time.Now().Format("00:00:00") + "] - " + name + " login \n"
+		_, err := curConn.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("failed to remind %s new user loging", connMap[curConn])
+			continue
+		}
+	}
 }
 
-///notify all user someone logout
+// /notify all user someone logout
 func notifyAllUserLogout(name string, connMap map[net.Conn]string, t int) {
-    for curConn := range connMap {
-        msg := "[" +time.Now().Format("00:00:00") + "] - " + name 
-        if t == 1 {
-            msg = msg + " logout \n"
-        } else if t == 2 {
-            msg = msg + " timeout \n"
-        }
-        _, err := curConn.Write([]byte(msg))
-        if err != nil {
-            fmt.Printf("failed to remind %s someone logout", connMap[curConn])
-            continue
-        }
-    }
+	for curConn := range connMap {
+		msg := "[" + time.Now().Format("00:00:00") + "] - " + name
+		if t == 1 {
+			msg = msg + " logout \n"
+		} else if t == 2 {
+			msg = msg + " timeout \n"
+		}
+		_, err := curConn.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("failed to remind %s someone logout", connMap[curConn])
+			continue
+		}
+	}
 }
 
-/// handle msg
+// / handle msg
 func handleMsg(conn net.Conn, connMap map[net.Conn]string, msg string) {
-    parseArr := strings.Split(msg, "|")
-    if len(parseArr) > 1 && parseArr[0] == "func" {
-        switch parseArr[1] {
-        case "rename":
-            {
-                if len(parseArr) == 3 {
-                    rename(conn, connMap, parseArr[2])
-                } else {
-                    syntaxError(conn)
-                }
-            }
-        case "list":
-            {
-                if len(parseArr) == 2 {
-                    list(conn, connMap)
-                } else {
-                syntaxError(conn)
-                }
-            }
-        case "exit":
-            {
-                if len(parseArr) == 2 {
-                    userExit(conn, connMap, 1)
-                } else {
-                    syntaxError(conn)
-                }
-            }
-        }
-    } else {
-        broadcast(conn, connMap, msg)
-    }
+	parseArr := strings.Split(msg, "|")
+	if len(parseArr) > 1 && parseArr[0] == "func" {
+		switch parseArr[1] {
+		case "rename":
+			{
+				if len(parseArr) == 3 {
+					rename(conn, connMap, parseArr[2])
+				} else {
+					syntaxError(conn)
+				}
+			}
+		case "list":
+			{
+				if len(parseArr) == 2 {
+					list(conn, connMap)
+				} else {
+					syntaxError(conn)
+				}
+			}
+		case "exit":
+			{
+				if len(parseArr) == 2 {
+					userExit(conn, connMap, 1)
+				} else {
+					syntaxError(conn)
+				}
+			}
+		}
+	} else {
+		broadcast(conn, connMap, msg)
+	}
 }
 
 func broadcast(conn net.Conn, connMap map[net.Conn]string, msg string) {
-    for curConn := range connMap {
-        msg:= "[" +time.Now().Format("00:00:00") + "] - " + connMap[conn] + ": " + msg + "\n"
-        _, err := curConn.Write([]byte(msg))
-        if err != nil {
-            fmt.Printf("failed to send msg to  %s", connMap[curConn])
-            continue
-        }
-    }
+	for curConn := range connMap {
+		msg := "[" + time.Now().Format("2006-01-02 15:04:05") + "] - " + connMap[conn] + ": " + msg + "\n"
+		_, err := curConn.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("failed to send msg to  %s", connMap[curConn])
+			continue
+		}
+	}
 }
 
-/// logout
+// / logout
 func userExit(conn net.Conn, connMap map[net.Conn]string, t int) {
-    name, exist := connMap[conn]
-    if exist {
-        delete(connMap, conn)
-        notifyAllUserLogout(name, connMap, t)
-    }
+	name, exist := connMap[conn]
+	if exist {
+		delete(connMap, conn)
+		notifyAllUserLogout(name, connMap, t)
+	}
 }
 
 func syntaxError(conn net.Conn) {
-    _, err := conn.Write([]byte("syntaxError \n"))
-    if err != nil {
-        fmt.Printf("failed to send msg to %s, cause syntaxError", conn)
-        return
-    }
+	_, err := conn.Write([]byte("syntaxError \n"))
+	if err != nil {
+		fmt.Printf("failed to send msg to %s, cause syntaxError", conn)
+		return
+	}
 }
 
-
 func rename(conn net.Conn, connMap map[net.Conn]string, newName string) {
-    _, err := conn.Write([]byte("success \n"))
-    if err != nil {
-        fmt.Printf("failed to rename %s", conn)
-        return
-    }
-    connMap[conn] = newName
+	_, err := conn.Write([]byte("success \n"))
+	if err != nil {
+		fmt.Printf("failed to rename %s", conn)
+		return
+	}
+	connMap[conn] = newName
 }
 
 func list(conn net.Conn, connMap map[net.Conn]string) {
-    msg := "------------------\n"
-    msg += "User List: \n"
-    for curConn, name:= range connMap {
-        
-        if conn != curConn {
-            msg += name + "\n"
-        }
-    }
-    msg += "------------------\n"
-    _, err := conn.Write([]byte(msg))
-    if err != nil {
-        fmt.Printf("failed to send user list to %s", conn)
-        return
-    }
+	msg := "------------------\n"
+	msg += "User List: \n"
+	for curConn, name := range connMap {
+
+		if conn != curConn {
+			msg += name + "\n"
+		}
+	}
+	msg += "------------------\n"
+	_, err := conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("failed to send user list to %s", conn)
+		return
+	}
 }
 
 func help(conn net.Conn) {
-    msg := `
+	msg := `
     --------------------------------------------------------------------------------------
     Introduction:
     This is a easy chat room!
@@ -206,38 +213,9 @@ func help(conn net.Conn) {
     - go offline
     --------------------------------------------------------------------------------------
     `
-    _, err := conn.Write([]byte(msg))
-    if err != nil {
-        fmt.Printf("failed to send msg to %s", conn)
-        return
-    }
+	_, err := conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("failed to send msg to %s", conn)
+		return
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
